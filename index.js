8234dda4e0ba07d7a968d7ca7f0a76a5ff2b4fe0
Mark <mark.nyambane@student.moringaschool.com>
2023-03-29 22:34:23 +0300

    done

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  function reverseWord(word) {
+    return word.toString().split("").reverse().join("");
+  }
+
+  const reversedWord = reverseWord(word)
+  if (word.toLowerCase() === reversedWord.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
 
 /* 
   Add your pseudocode here
+
+   function that takes in a word
+    compares that word with the word written backwards
+    if the word is the same backwards
+      return true
+    else
+      return false
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+   Two functions are used to check if the word is a palindrome
+  One function converts the word into a string in which each letter is split into an array, that array is reversed then converted back to a string
+  The second function takes the inputted word, compares it with the same word reversed by the other function and outputs true or false depending on whether the word is a palindrome or not
+  The function also accounts for whether the first letter is capitalized so as not to return false when a palindrome is entered in that way
+  
 */
 
 // You can run `node index.js` to view these console logs
